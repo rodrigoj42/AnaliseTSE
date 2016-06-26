6ffa0c4568dea849d89999b33af4665b6a635144
Yago Meira Lopes de Carvalho <yago.meira@poli.ufrj.br>
2016-06-26 16:59:53 -0300

    Added a html template for the web app

--- hello-flask.py
+++ hello-flask.py
@@ -2,19 +2,11 @@ from flask import *
 app = Flask(__name__)
 
 @app.route('/')
+def root():
+    return redirect(url_for('home'))
+@app.route('/home')
 def home():
-    return redirect(url_for('login'))
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login_form()
-def show_the_login_form():
-    return send_from_directory('index.html')
-def do_the_login():
-    return 'Login realizado'
-
-
+    return render_template('index.html')
+    #return 'Main page'
 if __name__ == '__main__':
     app.run('0.0.0.0')
