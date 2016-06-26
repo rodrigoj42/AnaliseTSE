e05536706c44324b85a74cd5c1b620b0ab8c4226
Yago Meira Lopes de Carvalho <yago.meira@poli.ufrj.br>
2016-06-26 03:45:01 -0300

    Updated hello-flask and added static folder

--- hello-flask.py
+++ hello-flask.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-@app.route('/AnaliseTSE/login')
+def home():
+    return redirect(url_for('login'))
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<html><head></head><body><h1>Testando as rotas</h1><h2>Essa &eacute; a an&aacute;lise do TSE</h2></body></html>'
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+def show_the_login_form():
+    return send_from_directory('index.html')
+def do_the_login():
+    return 'Login realizado'
 
-@app.route('/nathaliaeyago')
-def eu_te_amo():
-    return 'Amor, eu te amo muito!!!!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0')
