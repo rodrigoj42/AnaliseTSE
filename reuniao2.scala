43163e7c7b0e2b5271c449a97d58e67a363b2892
Rafael Damasceno <Damasceno@MacBook-Pro-de-Rafael.local>
2016-06-27 13:03:30 -0300

    reduceByKey

--- reuniao2.scala
+++ reuniao2.scala
@@ -1,6 +1,6 @@
 // CONTAGEM DOS VOTOS
 // ==================
-val votos = sc.textFile("file:/Users/rodrigoj42/Downloads/bweb/*.txt").filter(e => e.length > 0).map(e => e.split("\";\""))
+val votos = sc.textFile("file:/home/geonumerica/Desktop/bweb/*.txt").filter(e => e.length > 0).map(e => e.split("\";\""))
 
 //  Legenda Boletim Web
 //  0: data
@@ -46,7 +46,7 @@ val votosPorcentagem = votosPresidenteMapeada.join(numVotosPorSecao).map(e => (e
 
 // PERFIL DOS ELEITORES   
 // ====================
-val eleitores = sc.textFile("file:/Users/rodrigoj42/Downloads/perfil_eleitor/*.txt").filter(e => e.length > 0).map(e => e.split("\";\"")) 
+val eleitores = sc.textFile("file:/home/geonumerica/Desktop/perfil_eleitor/*.txt").filter(e => e.length > 0).map(e => e.split("\";\"")) 
 
 //  Legenda Perfil Eleitorado
 //  0: data
@@ -81,14 +81,31 @@ val faixaEtariaCombinada  =  faixaEtaria.map(e => (e._1._1, (e._1._2, e._2))).jo
 val escolaridadeCombinada = escolaridade.map(e => (e._1._1, (e._1._2, e._2))).join(eleitoresSecao)
 val sexoCombinado         =         sexo.map(e => (e._1._1, (e._1._2, e._2))).join(eleitoresSecao)
 
-val estadoCivilPorcentado  =  estadoCivilCombinado.map(e => ((e._1, e._2._1._1), e._2._1._2.toFloat*100/e._2._2))
-val faixaEtariaPorcentada  =  faixaEtariaCombinada.map(e => ((e._1, e._2._1._1), e._2._1._2.toFloat*100/e._2._2))
-val escolaridadePorcentada = escolaridadeCombinada.map(e => ((e._1, e._2._1._1), e._2._1._2.toFloat*100/e._2._2))
-val sexoPorcentado         =         sexoCombinado.map(e => ((e._1, e._2._1._1), e._2._1._2.toFloat*100/e._2._2))
+val estadoCivilPorcentado  =  estadoCivilCombinado.map(e => (e._1, (e._2._1._1, e._2._1._2.toFloat*100/e._2._2)))
+val faixaEtariaPorcentada  =  faixaEtariaCombinada.map(e => (e._1, (e._2._1._1, e._2._1._2.toFloat*100/e._2._2)))
+val escolaridadePorcentada = escolaridadeCombinada.map(e => (e._1, (e._2._1._1, e._2._1._2.toFloat*100/e._2._2)))
+val sexoPorcentado         =         sexoCombinado.map(e => (e._1, (e._2._1._1, e._2._1._2.toFloat*100/e._2._2)))
 
 // possibilidade 2 => 1 rdd geral, depois so fazer filter + reduce, talvez mais facil pra combinar
 val geral = eleitores.map(e => (e(6) + "." + e(7),(e(9), e(11), e(13), e(15), e(16).dropRight(1).toInt)))
 val homensPorSecao = geral.map(e => ((e._1, e._2._4),e._2._5)).filter(e => e._2._1 == "MASCULINO").reduceByKey((a,b) => a + b)
+
+
+//=====> novo begin
+
+val votosMax = votosPorcentagem.map(e => (e._1, (e._2, e._3))).reduceByKey((a, b) => (if (a._2 > b._2) a else b))
+
+val estadoCivilMax  =  estadoCivilPorcentado.reduceByKey((a, b) => (if (a._2 > b._2) a else b))
+val faixaEtariaMax  =  faixaEtariaPorcentada.reduceByKey((a, b) => (if (a._2 > b._2) a else b))
+val escolaridadeMax = escolaridadePorcentada.reduceByKey((a, b) => (if (a._2 > b._2) a else b))
+val sexoMax 	    =         sexoPorcentado.reduceByKey((a, b) => (if (a._2 > b._2) a else b))
+
+val votosEstadoCivil  =  votosMax.join(estadoCivilMax).sortBy(e => e._2._2._2, false)
+val votosFaixaEtaria  =  votosMax.join(faixaEtariaMax).sortBy(e => e._2._2._2, false)
+val votosEscolaridade = votosMax.join(escolaridadeMax).sortBy(e => e._2._2._2, false)
+val votosSexo         =         votosMax.join(sexoMax).sortBy(e => e._2._2._2, false)
+
+//=====> novo end
 
 
 
