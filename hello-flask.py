fa22875d00a505ccc6e9132520e96722385e5fa1
Yago Meira Lopes de Carvalho <yago.meira@poli.ufrj.br>
2016-06-27 01:25:03 -0300

    Updated some stuff on the template

--- hello-flask.py
+++ hello-flask.py
@@ -7,6 +7,14 @@ def root():
 @app.route('/home')
 def home():
     return render_template('index.html')
-    #return 'Main page'
+@app.route('/item1')
+def item1():
+    return render_template('item.html')
+@app.route('/item2')
+def item2():
+    return render_template('item.html')
+@app.route('/item3')
+def item3():
+    return render_template('item.html')
 if __name__ == '__main__':
     app.run('0.0.0.0')
