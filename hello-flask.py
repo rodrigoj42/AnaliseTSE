18af6658d7e909fa643dad20ddf089c0636ec1c7
Yago Meira Lopes de Carvalho <yago.meira@poli.ufrj.br>
2016-06-28 07:48:39 -0300

    Modified template

--- hello-flask.py
+++ hello-flask.py
@@ -1,18 +1,23 @@
 from flask import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return redirect(url_for('home'))
+
 @app.route('/home')
 def home():
     return render_template('index.html')
+
 @app.route('/item1')
 def item1():
     return render_template('item.html')
+
 @app.route('/item2')
 def item2():
     return render_template('item.html')
+
 @app.route('/item3')
 def item3():
     return render_template('item.html')
